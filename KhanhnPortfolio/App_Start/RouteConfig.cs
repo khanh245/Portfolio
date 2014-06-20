ad56c92274551587ccc7c61cf86871ca4428aeda
Khanh Nguyen <knultimate@live.com>
2014-06-20 00:22:28 -0700

    Added Meet Us Controller and View

--- KhanhnPortfolio/App_Start/RouteConfig.cs
+++ KhanhnPortfolio/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace KhanhnPortfolio
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "Contact",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Contact", action = "MeetUs", id = UrlParameter.Optional }
+            );
         }
     }
 }
---
+++ KhanhnPortfolio/Controllers/ContactController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace KhanhnPortfolio.Controllers
+{
+    public class ContactController : Controller
+    {
+
+        public ActionResult MeetUs()
+        {
+            return View();
+        }
+    }
+}
